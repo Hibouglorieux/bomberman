23d2596d950cac26cfabca2688bc679fdc9049c5
Hubert <hubertvromman1@Shamry.local>
2019-07-25 19:42:30 +0200

    mouvement avec collision, mur cassable avec index 2, stocke dans var mur

--- manage_mvt.js
+++ manage_mvt.js
@@ -2,28 +2,60 @@ function movement()
 {
     if (key_up.isDown)
     {
-        Players.W_player.y -= speed;
+    	for (var i = 0; i < speed; i++)
+    	{
+    		Players.W_player.y -= 1;
+    		if (get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[2]) != 0 || get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[3]) != 0)
+    		{
+    			Players.W_player.y += 1;
+    			// break;
+    		}
+    	}
 		if (Players.W_player.face != "up" || !Players.W_player.anims.isPlaying)
 		Players.W_player.play('w_up');
 		Players.W_player.face = "up"
     }
 	else if (key_bottom.isDown)
     {
-        Players.W_player.y += speed;
+        for (var i = 0; i < speed; i++)
+    	{
+    		Players.W_player.y += 1;
+    		if (get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[0]) != 0 || get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[1]) != 0)
+    		{
+    			Players.W_player.y -= 1;
+    			// break;
+    		}
+    	}
 		if (Players.W_player.face != "down" || !Players.W_player.anims.isPlaying)
 		Players.W_player.play('w_down');
 		Players.W_player.face = "down"
     }
     else if (key_left.isDown)
     {
-        Players.W_player.x -= speed;
+    	for (var i = 0; i < speed; i++)
+    	{
+    		Players.W_player.x -= 1;
+    		if (get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[3]) != 0 || get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[1]) != 0)
+    		{
+    			Players.W_player.x += 1;
+    			// break;
+    		}
+    	}
 		if (Players.W_player.face != "left" || !Players.W_player.anims.isPlaying)
 		Players.W_player.play('w_left');
 		Players.W_player.face = "left"
     }
 	else if (key_right.isDown)
     {
-        Players.W_player.x += speed;
+        for (var i = 0; i < speed; i++)
+    	{
+    		Players.W_player.x += 1;
+    		if (get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[2]) != 0 || get_case(get_player_hitbox(Players.W_player.x, Players.W_player.y)[0]) != 0)
+    		{
+    			Players.W_player.x -= 1;
+    			// break;
+    		}
+    	}
 		if (Players.W_player.face != "right" || !Players.W_player.anims.isPlaying)
 		Players.W_player.play('w_right');
 		Players.W_player.face = "right"
