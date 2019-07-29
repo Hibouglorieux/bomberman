da1423e1d4293247dfbcf48b32121b3748c884ad
hvromman <hvromman@debian.roger.skyline>
2019-07-29 18:39:29 -0400

    server go

--- server/server_main.go
+++ server/server_main.go
@@ -2,54 +2,38 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-
-	socketio "github.com/googollee/go-socket.io"
+//	"io/ioutil"
+	"log"
+	"bytes"
+//	"html"
 )
 
-func main() {
-	mainserver, err := socketio.NewServer(nil)
-	if err != nil {
-		fmt.Println("fatal error with NewServer", err)
-		log.Fatal(err)
-	}
-	mainserver.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
-		fmt.Println("connected:", s.ID())
-		return nil
-	})
+//var INDEX_HTML []byte
+//var value string = "hell"
 
-	mainserver.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		s.Emit("reply", "have "+msg)
-	})
-
-	mainserver.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-
-	mainserver.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-
-	mainserver.OnError("/", func(e error) {
-		fmt.Println("meet error:", e)
-	})
-	mainserver.OnDisconnect("/", func(s socketio.Conn, msg string) {
-		fmt.Println("closed", msg)
-	})
-	go mainserver.Serve()
-	defer mainserver.Close()
-
-	http.Handle("/socket.io/", mainserver)
-	http.Handle("/", http.FileServer(http.Dir("..")))
-//	http.Handle("/socket.io/", mainserver)
-//	http.Handle("/", http.FileServer(http.Dir("./asset")))
-	log.Println("Serving at localhost:8000...")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+func main(){
+	fmt.Printf("starting server on http://localhost:8000/\n")
+//	http.HandleFunc("/", IndexHandler)
+	fs := http.FileServer(http.Dir(".."))
+	http.Handle("/", fs)
+	http.HandleFunc("/post", PostHandler)
+	http.ListenAndServe(":8000", nil)
 }
+
+//func IndexHandler(w http.ResponseWriter, r *http.Request){
+//	log.Println("GET /")
+//	w.Write(INDEX_HTML)
+//}
+
+func PostHandler(w http.ResponseWriter, r *http.Request){
+//	r.ParseForm()
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	log.Printf("hello Nathan %s\n", buf.String())
+//	var value = r.FormValue("textfield")
+	w.Write([]byte("hell"))
+}
+//func init(){
+//	INDEX_HTML, _ = ioutil.ReadFile("../index.html")
+//}
